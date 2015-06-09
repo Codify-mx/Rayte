f53d4c0c33aa9b2f34e19339bb9f713fe1d9d308
LaloObz <LaloObz@af1317fc-3b4c-4669-97ed-80e39ed12a5e>
2015-06-09 04:41:48 +0000

    Centrar mapa en posición Crea marcador en la posicón del dispositivo

--- trunk/geolocation/platforms/android/assets/www/js/gps.js
+++ trunk/geolocation/platforms/android/assets/www/js/gps.js
@@ -1,3 +1,61 @@
+
+function GPS(){
+    this.map;
+    this.watchId = 0;
+    
+    this.initialize = function(){
+        console.log('initialize');
+        this.bindEvents();
+    };
+    this.bindEvents = function(){
+        console.log('bindEvents');
+        document.addEventListener('deviceready',this.onDeviceReady, false);
+    };
+    this.cancelWatch = function(){
+        console.log(this.watchID );
+        navigator.geolocation.clearWatch(this.watchID );
+        this.agregaMensaje('terminado');
+    };
+    this.centrarMapa = function(){
+        console.log('centrar');
+        navigator.geolocation.getCurrentPosition(this.onSuccessCenter, this.onError);
+    };
+    this.onSuccessCenter = function(position){
+        this.gregaMensaje('Lat: '+position.coords.latitude+' Lng: '+position.coords.longitude);
+        var myLatlng = new google.maps.latlng(position.coords.latitude,position.coords.longitude);
+        map.setCenter(myLatlng);
+        map.setZoom(11); 
+    };
+    
+    this.onDeviceReady = function() {
+        this.agregaMensaje('Listo');
+        //document.getElementById('fetch').onclick = GPS.getPosition();
+        this.watchID = navigator.geolocation.watchPosition(this.onSuccess,this.onError);
+        console.log(this.watchID );
+    }
+    
+    this.onSuccess = function(position) {
+        this.agregaMensaje('Lat: '+position.coords.latitude+' Lng: '+position.coords.longitude);
+    }
+    this.onError =function (error) {
+       this.agregaMensaje(error);
+    }
+    
+    var latlng =[];
+    this.agregaMensaje = function (mensaje){
+        console.log(mensaje);
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(mensaje);         // Create a text node
+        node.appendChild(textnode);
+        latlng.push(node);// Append the text to <li>
+        document.getElementById("position").appendChild(node);
+    }
+    
+    
+}
+
+var gps = new GPS();
+/*
 var GPS ={
     watchID: 0,
     getPosition : getPosition,
@@ -16,20 +74,16 @@ var GPS ={
         agregaMensaje('terminado');
     },
     centerMap: function(){
-       navigator.geolocation.getCurrentPosition(GPS.onSuccessCenter, GPS.onErrorCenter);
+        console.log('centrar');
+        navigator.geolocation.getCurrentPosition(GPS.onSuccessCenter, GPS.onError);
     },
-    onSuccess : function(){
+    onSuccessCenter : function(position){
           agregaMensaje('Lat: '+position.coords.latitude+' Lng: '+position.coords.longitude);
           var myLatlng = new google.maps.latlng(position.coords.latitude,position.coords.longitude);
-          var myOptions = {
-                center: myLatlng,
-                zoom: 16,
-                mapTypeId: google.maps.mapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+          map.setCenter(myLatlng);
+          map.setZoom(11);
             
-        },
-    onErrorCenter : onError
+        }
 }
     
     function onDeviceReady() {
@@ -40,7 +94,6 @@ var GPS ={
     }
     function getPosition() {
         //navigator.geolocation.getCurrentPosition(GPS.onSuccess, GPS.onError);
-       
         console.log('deviceready');
     }
     
@@ -61,3 +114,4 @@ var GPS ={
         document.getElementById("position").appendChild(node);
         
     }
+*/
