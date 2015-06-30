c837de1c4483a1647b4933fad28d5a7dd75e4113
LaloObz <LaloObz@af1317fc-3b4c-4669-97ed-80e39ed12a5e>
2015-06-30 23:48:40 +0000

    validaciones pedir taxi, codificar direccion, conexion a internet login diseño

--- trunk/rayte/www/js/soap.js
+++ trunk/rayte/www/js/soap.js
@@ -105,7 +105,8 @@ var soap = {
                 }
             },
             error: function (data) {
-                console.log(data)
+                console.log(data);
+                error(data);
             }
         });
     }
